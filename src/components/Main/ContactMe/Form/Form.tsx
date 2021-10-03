bdd75891a6af7d15a4ccdac011e807ec15b0a960
aportraitofjoyce <aportraitofjoyce@gmail.com>
2021-10-04 02:28:12 +0300

    Form submit response to email with EmailjS library

--- src/components/Main/ContactMe/Form/Form.tsx
+++ src/components/Main/ContactMe/Form/Form.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import s from '../ContactMe.module.css'
+import emailjs from 'emailjs-com'
 
 export const Form = () => {
-    const onSendButtonClick = (e: any) => {
+    const onSubmit = async (e: any) => {
         e.preventDefault()
-        alert('Thank you!')
+        try {
+            const response = await emailjs.sendForm('gmail', 'template_7eyueer', e.target, 'user_22VRdmcJd0uaKf5W0EuQ4')
+            alert(response.text)
+            e.target.reset()
+        } catch (e) {
+            alert(e.text)
+        }
     }
 
     return (
-        <form action="" className={s.formContainer}>
-            <input type="text" placeholder={'Name'}/>
-            <input type="text" placeholder={'Email'}/>
-            <textarea name="" id="" placeholder={'Say something about your project...'}/>
-            <button onClick={onSendButtonClick}>Send</button>
+        <form onSubmit={onSubmit} className={s.formContainer}>
+            <input type='text' name={'name'} placeholder={'Name'}/>
+            <input type='text' name={'email'} placeholder={'Email'}/>
+            <textarea name='message' placeholder={'Say something about your project...'}/>
+            <button type={'submit'}>Send</button>
         </form>
     )
 }
